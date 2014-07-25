10873f6274cfbcfdae633e0ea125963bea96d143
xioTechnologies <social@x-io.co.uk>
2014-07-25 02:02:29 +0100

    Add missing file FirmwareVersion.h

---
+++ Firmware/FirmwareVersion.h
@@ -0,0 +1,17 @@
+/*
+ * File:   FirmwareVersion.h
+ * Author: Seb Madgwick
+ */
+
+#ifndef FIRMWARE_VERSION_H
+#define FIRMWARE_VERSION_H
+
+//------------------------------------------------------------------------------
+// Definitions
+
+#define FIRMWARE_VERSION "vX.X"
+
+#endif
+
+//------------------------------------------------------------------------------
+// End of file
